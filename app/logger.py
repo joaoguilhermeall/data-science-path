b92828c9da02b5249daf3b68c382175606915efd
João Guilherme Ubuntu WSL <joaoguilhermeall@gmail.com>
2021-12-12 03:24:06 -0300

    Set level VERBOSE=25 and add function verbose

--- app/logger.py
+++ app/logger.py
@@ -1,9 +1,27 @@
 import logging
+from re import VERBOSE
 
-class Logger():
+VERBOSE = 25
+
+
+def verbose(msg: str) -> None:
+    """Verbose execution
+
+    Args:
+        msg (str): Message to show on verbose
+    """
+    logging.log(msg, VERBOSE)
+
+
+class Logger:
     """Define a Logger constructor"""
-    def __init__(self) -> None:
-        pass
+
+    def __init__(self, level: int = logging.WARNING, verbose: bool = False) -> None:
+        if verbose:
+            logging.addLevelName(VERBOSE, "VERBOSE")
+
+        self._formatter = logging.Formatter()
+        self._logger = logging.getLogger(name="Data Science Path - Logger")
 
     def run(level: int = logging.DEBUG) -> None:
         pass
