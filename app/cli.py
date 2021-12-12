c8e9df71b3fdd9705fd7080523f2a72676e13fcd
João Guilherme Ubuntu WSL <joaoguilhermeall@gmail.com>
2021-12-12 04:48:40 -0300

    Add option to require kaggle credentials

--- app/cli.py
+++ app/cli.py
@@ -5,21 +5,29 @@ def cli_constructor() -> None:
 
     parser = argparse.ArgumentParser(
         prog="Data Science Path - CLI",
-        description="Run a specific routines of Pipeline Application"
+        description="Run a specific routines of Pipeline Application",
     )
-    
+
     parser.add_argument(
         "action",
+        required=True,
         choices=["extract", "transform", "load", "pipeline"],
         help="Level of Application",
     )
     parser.add_argument(
-        "--verbose", 
         "-v",
+        "--verbose",
         help="Be verbose",
         action="store_true",
         type=bool,
-        default=False
+        default=False,
+    )
+    parser.add_argument(
+        "--kaggle",
+        help="Require Username and API Key of Kaggle from prompt",
+        type=bool,
+        action="store_true",
+        default=False,
     )
 
     args = parser.parse_args()
