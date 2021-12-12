a49e83cd0ca9dd48121cadd17f2b6376e76cb11c
João Guilherme Ubuntu WSL <joaoguilhermeall@gmail.com>
2021-12-12 03:02:16 -0300

    Level to Action. Add --verbose -v option

--- app/cli.py
+++ app/cli.py
@@ -3,16 +3,27 @@ import argparse
 
 def cli_constructor() -> None:
 
-    parser = argparse.ArgumentParser(prog="App CLI")
+    parser = argparse.ArgumentParser(
+        prog="Data Science Path - CLI",
+        description="Run a specific routines of Pipeline Application"
+    )
     
     parser.add_argument(
-        "level",
+        "action",
         choices=["extract", "transform", "load", "pipeline"],
         help="Level of Application",
+    )
+    parser.add_argument(
+        "--verbose", 
+        "-v",
+        help="Be verbose",
+        action="store_true",
+        type=bool,
+        default=False
     )
 
     args = parser.parse_args()
 
 
 if __name__ == "__main__":
-    ...
+    cli_constructor()
