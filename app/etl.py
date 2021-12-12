1c9f0a444b6c7175805e8157da33ea3534c0124b
João Guilherme Ubuntu WSL <joaoguilhermeall@gmail.com>
2021-12-12 04:52:20 -0300

    Create a simple ETL class

---
+++ app/etl.py
@@ -0,0 +1,49 @@
+from typing import Any
+import kaggle
+
+from abc import ABC
+
+from app.logger import verbose
+from app.config import BaseConfig
+
+
+class ActionETLException(BaseException):
+    pass
+
+
+class ETL(ABC):
+    """Base ETL class"""
+
+    def __init__(self, configs: BaseConfig) -> None:
+        super().__init__()
+        self._configs = configs
+
+    def __getattribute__(self, name: str) -> Any:
+        if name == "extract":
+            return self.extract
+        elif name == "transform":
+            return self.transform
+        elif name == "loading":
+            return self.loading
+        elif name == "pipeline":
+            return self.pipeline
+        else:
+            raise (ActionETLException(f"Action for ETL is not valid: {name}"))
+
+    def extract(self):
+        # TODO: Build extract dataset from kaggle
+        kaggle.api.authenticate()
+
+        pass
+
+    def transform(self):
+        pass
+
+    def loading(self):
+        pass
+
+    def pipeline(self):
+
+        self.extract()
+        self.transform()
+        self.loading()
