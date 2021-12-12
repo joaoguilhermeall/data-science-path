2dddded557634500f910101078201ebe9e0dac6a
João Guilherme Ubuntu WSL <joaoguilhermeall@gmail.com>
2021-12-12 03:00:13 -0300

    Start build Logger constructor

---
+++ app/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+class Logger():
+    """Define a Logger constructor"""
+    def __init__(self) -> None:
+        pass
+
+    def run(level: int = logging.DEBUG) -> None:
+        pass
