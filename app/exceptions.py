900c3057e9c33614631a6539c97ad90a432863f7
João Guilherme Ubuntu WSL <joaoguilhermeall@gmail.com>
2021-12-22 19:47:03 -0300

    Add to manager all app exceptions

---
+++ app/exceptions.py
@@ -0,0 +1,25 @@
+# Exceptions of App
+
+class _KaggleException(BaseException):
+    """Exceptions involving Kaggle functionality"""
+    pass
+
+
+class KaggleKeysFile(_KaggleException):
+    """Kaggle file API keys not founded on app"""
+    pass
+
+
+class KaggleKeysEnviron(_KaggleException):
+    """Kaggle API keys not founded on environ app"""
+    pass
+
+
+class _ETLExceptions(BaseException):
+    """Base Exceptions of ETL execution"""
+    pass
+
+
+class LevelETLException(_ETLExceptions):
+    """Level not defined to run"""
+    pass
