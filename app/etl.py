d91cad1c9d2b2db7480771e9c7bcbd4dc1903f4f
João Guilherme Ubuntu WSL <joaoguilhermeall@gmail.com>
2021-12-22 21:25:23 -0300

    ETL class suitable for a Configs formats

--- app/etl.py
+++ app/etl.py
@@ -1,23 +1,20 @@
 from pathlib import Path
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, Union
 
-import os
 from abc import ABC
 
 from pandas import DataFrame, read_csv
 
 from app.logger import verbose
-from app.config import BaseConfig, KaggleException
+from app.config import AppConfig
 
-
-class ActionETLException(BaseException):
-    pass
+from app.exceptions import KaggleTokenInvalid, LevelETLException
 
 
 class ETL(ABC):
     """Base ETL class"""
 
-    def __init__(self, configs: BaseConfig) -> None:
+    def __init__(self, configs: AppConfig) -> None:
         super().__init__()
         self._configs = configs
         self._dataframes: Dict[str, DataFrame] = {}
@@ -32,7 +29,7 @@ class ETL(ABC):
         elif name == "pipeline":
             return self.pipeline
         else:
-            raise (ActionETLException(f"Action for ETL is not valid: {name}"))
+            raise (LevelETLException(f"ETL level is not valid: {name}"))
 
     def _download_dataset(self, dataset: str, filename: str) -> Path:
         """Download dataset file from Kaggle"""
@@ -52,41 +49,45 @@ class ETL(ABC):
             )
         except ApiException(401) as ex:
             raise (
-                KaggleException(f"{ex}: Please, try a new Kaggle API Token. Manages it on the kaggle website")
+                KaggleTokenInvalid(f"{ex}: Please, try a new Kaggle API Token. Manages it on the kaggle website")
             )
         
         return self._configs.KAGGLE_DATASET_FILE
 
     def extract(self) -> None:
         """Extract and build information"""
-        dataset = self._configs.KAGGLE_DATASET
-        filename = self._configs.KAGGLE_DATASET_FILENAME
-        file_key = filename.name.rsplit(".")[0]
-
         # If dataset file not exist or force download
         if not self._configs.KAGGLE_DATASET_FILE.exists():
             verbose("File does not exist. Downloading")
-            file_path = self._download_dataset(dataset, filename)
+            file_path = self._download_dataset()
             
         elif self._configs.KAGGLE_DOWNLOAD_FORCE:
             verbose("Force download!")
-            file_path = self._download_dataset(dataset, filename)
+            file_path = self._download_dataset()
         else:
             pass
 
+        file_key = file_path.name.rsplit(".")[0]
+
+        # If necessary, build another Dataframes here
         self._dataframes[file_key] = read_csv(file_path)
 
+        verbose("Extraction finished!")
+
     def transform(self):
+        # TODO: ETL TRANSFORM
         pass
 
     def loading(self):
+        # TODO: ETL LOADING
         pass
 
     def pipeline(self):
-
+        """Run all steps of ETL"""
         self.extract()
         self.transform()
         self.loading()
 
-    def dataframe(self) -> Union[DataFrame, None]:
+    @property
+    def dataframes(self) -> Union[DataFrame, None]:
         return self._dataframes
