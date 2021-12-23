4b12cb4efd22c7e8f7aac184ba3960651304cfeb
João Guilherme Ubuntu WSL <joaoguilhermeall@gmail.com>
2021-12-22 21:22:28 -0300

    Change CLI from fun to complete class

--- app/cli.py
+++ app/cli.py
@@ -1,43 +1,149 @@
+from getpass import getpass
+from os import environ
+from re import S
+from typing import Union, Dict
+
+from pathlib import Path
+from json import loads
 import argparse
 
+from app.config import AppConfig
+from app.exceptions import KaggleKeysEnviron, KaggleKeysFile
 
-def cli_constructor() -> argparse.Namespace:
 
-    parser = argparse.ArgumentParser(
-        prog="Data Science Path - CLI",
-        description="Run a specific routines of Pipeline Application",
-    )
+class CLI:
+    def __init__(self, configs: AppConfig) -> None:
+        self._configs = configs
 
-    parser.add_argument(
-        "action",
-        choices=["extract", "transform", "load", "pipeline"],
-        help="Level of Application",
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        help="Be verbose",
-        action="store_true",
-        default=False,
-    )
-    parser.add_argument(
-        "--kaggle",
-        help="Require Username and API Key of Kaggle if dataset was not downloaded yet",
-        action="store_true",
-        default=False,
-    )
-    parser.add_argument(
-        "-f",
-        "--force",
-        help="Force Kaggle Request credentials and download dataset even if it already exists",
-        action="store_true",
-        default=False,
-    )
+        self._build_cli()
+        self._run_cli()
 
-    args = parser.parse_args()
+    def _build_cli(self) -> None:
+        """Build all CLI options"""
 
-    return args
+        self._parser = argparse.ArgumentParser(
+            prog="APP",
+            description="Lucas's Data Science Path with Pima Indians Diabetes Database",
+            epilog="That's all, folks!\n",
+        )
+
+        self._parser.add_argument(
+            "level",
+            choices=["extract", "transform", "load", "pipeline"],
+            help="Application level to run",
+        )
+
+        self._parser.add_argument(
+            "--force",
+            "-f",
+            help="Force download dataset from Kaggle",
+            action="store_true",
+        )
+
+        self._parser.parse_args()
+
+    def _run_cli(self) -> None:
+        credentials = {}
+
+        credentials = self._get_kaggle_credentials()
+
+        self._export_kaggle_credentials(credentials)
+
+    @property
+    def args(self) -> argparse.Namespace:
+        """Return parseds arguments"""
+        return self._parser.parse_args()
+
+    @property
+    def configs(self) -> AppConfig:
+        return self._configs
+
+    @property
+    def level(self) -> str:
+        return self.args.level
+
+    @property
+    def verbose(self) -> str:
+        return self.args.verbose
+
+    def _get_kaggle_credentials_from_environ(self) -> Dict[str, str]:
+        credentials = {}
+
+        try:
+            credentials["KAGGLE_USERNAME"] = environ["KAGGLE_USERNAME"]
+            credentials["KAGGLE_KEY"] = environ["KAGGLE_KEY"]
+        
+        except KeyError as ke:
+            msg = " ".join(
+                [
+                    "There is no variables seted on environ.",
+                    f"Following key is resquest: {ke}.",
+                    "See how to run app on README file.",
+                ]
+            )
+            print(KaggleKeysEnviron(msg))
+
+        return credentials
+
+    def _get_kaggle_credentials_from_file(self) -> Dict[str, str]:
+        """Check if credential file API exists"""
+        credentials = {}
+        credentials_file = Path(self._configs.KEYS) / "kaggle.json"
+
+        if credentials_file.exists():
+            with open(credentials_file) as file:
+                credentials_dict = loads(file.read())
+
+            try:
+                credentials["KAGGLE_USERNAME"] = credentials_dict["username"]
+                credentials["KAGGLE_KEY"] = credentials_dict["key"]
+
+            except KeyError as ke:
+                msg = " ".join(
+                    [
+                        "There is no valid kaggle.json file on keys folder.",
+                        f"Following key is resquest: {ke}.",
+                        "See how to run app on README file or by options invoking [-h] flag.",
+                    ]
+                )
+                print(KaggleKeysFile(msg))
+        
+        return credentials
+
+    def _get_kaggle_credentials(self) -> Dict[str, str]:
+        credentials = self._get_kaggle_credentials_from_environ()
+
+        if credentials == {}:
+            credentials = self._get_kaggle_credentials_from_file()
+
+        if credentials == {}:
+            credentials = self._request_kaggle_credentials()
+
+        return credentials
+    
+    def _request_kaggle_credentials(self) -> Dict[str, str]:
+        """Request KAGGLE credentials"""
+
+        print("\nKaggle Credentials\n")
+        print("Credentials are being requested because the", end=" ")
+        print("configuration file was not found and the variables", end=" ")
+        print("were not found in the environment", end="\n\n")
+        
+        credentials = {}
+
+        credentials["KAGGLE_USERNAME"] = input("Enter Kaggle API Username: ")
+        credentials["KAGGLE_KEY"] = getpass("Enter Kaggle API Key: ")
+
+        return credentials
+
+    def _export_kaggle_credentials(self, credentials: Dict[str, str]) -> None:
+        """Export kaggle credentials to environ"""
+        environ["KAGGLE_USERNAME"] = credentials["KAGGLE_USERNAME"]
+        environ["KAGGLE_KEY"] = credentials["KAGGLE_KEY"]
+
+        self._configs.KAGGLE_USERNAME = credentials["KAGGLE_USERNAME"]
+        self._configs.KAGGLE_KEY = credentials["KAGGLE_KEY"]
 
 
 if __name__ == "__main__":
-    cli_constructor()
+    pass
